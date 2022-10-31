c1cd1268fa7b239d1c4bfa50d21bf8ccfae16b80
tekutoki1ari <borbotkodenis@gmail.com>
2022-10-31 20:59:51 +0200

    chat_bot 2-nd stage

--- src/chat_bot/chat_bot.java
+++ src/chat_bot/chat_bot.java
@@ -1,8 +1,19 @@
 package chat_bot;
 
+
+import java.util.Scanner;
+
+
 public class chat_bot {
-    public static void main (String[] args){
-        System.out.println("Hello! My name is 2iq_bot");
-        System.out.println("I was created in 2022");
+    public static void main (String[] args) {
+
+        System.out.println("Hello! My name is 2iq_bot.");
+        System.out.println("I was created in 2022.");
+        System.out.println("Please, remind me your name.");
+
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        name = scanner.nextLine();
+        System.out.println("What a great name you have, " + name);
     }
 }
