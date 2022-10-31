f935fef2517954bb49633b3c2f6a0b11099aa62a
tekutoki1ari <borbotkodenis@gmail.com>
2022-10-31 20:36:46 +0200

    chat_bot 1-st stage

---
+++ src/chat_bot/chat_bot.java
@@ -0,0 +1,8 @@
+package chat_bot;
+
+public class chat_bot {
+    public static void main (String[] args){
+        System.out.println("Hello! My name is 2iq_bot");
+        System.out.println("I was created in 2022");
+    }
+}
