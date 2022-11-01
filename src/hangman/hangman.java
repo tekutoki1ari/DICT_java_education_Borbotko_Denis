aced325928cdf7f66fd91e69580ed16d3b16b144
tekutoki1ari <borbotkodenis@gmail.com>
2022-11-02 00:21:36 +0200

    hangman stage 2

--- src/hangman/hangman.java
+++ src/hangman/hangman.java
@@ -1,8 +1,22 @@
 package hangman;
 
+
+import java.util.Scanner;
+
+
 public class hangman {
     public static void main(String[] args){
         System.out.println("HANGMAN");
         System.out.println("The game will be available soon.");
+        System.out.println("enter world: ")
+        String answerTrue = "java";
+        Scanner userInput = new Scanner(System.in);
+        String answer = userInput.nextLine();
+        if (answerTrue.equals(answer)){
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
+
     }
 }
