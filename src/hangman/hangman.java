cbebd181cfe05195dc6315446439d395bb58a1bd
tekutoki1ari <borbotkodenis@gmail.com>
2022-11-02 00:06:04 +0200

    hangman stage 1

---
+++ src/hangman/hangman.java
@@ -0,0 +1,8 @@
+package hangman;
+
+public class hangman {
+    public static void main(String[] args){
+        System.out.println("HANGMAN");
+        System.out.println("The game will be available soon.");
+    }
+}
