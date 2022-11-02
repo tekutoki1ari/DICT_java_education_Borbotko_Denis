1b1b35264de18fa0ee9dc7bd04959e9e7c3e1340
tekutoki1ari <borbotkodenis@gmail.com>
2022-11-02 17:27:29 +0200

    hangman stage 7

--- src/hangman/hangman.java
+++ src/hangman/hangman.java
@@ -1,7 +1,9 @@
 package hangman;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -10,7 +12,9 @@ public class hangman {
         System.out.println("HANGMAN");
         System.out.println("The game will be available soon.");
 
-        byte attemps = 8;
+        List<Character> used = new ArrayList<>();
+
+        byte attemps = 8; //attempts
 
         String[] dict = new String[]{"python", "java", "javascript", "kotlin"};
         String hiddenWord = dict[(int) (Math.random() * dict.length)];
@@ -22,21 +26,23 @@ public class hangman {
         while (attemps > 0) {
             System.out.printf("You life: %d\nGuess the word %s: ", attemps, String.valueOf(hiddenWordChar));
             char char_answer = userAnswer.nextLine().toCharArray()[0];
-            char[] hiddenWordChar_N = checkout(hiddenWordArray, hiddenWordChar.clone(), char_answer);
-            if (hiddenWordChar_N == null) {
-                attemps--;
-                System.out.println("That letter doesn`t appear in the word");
+            if (97 <= Character.hashCode(char_answer) & Character.hashCode(char_answer) <= 122) {
+                if (used.isEmpty() | !used.contains(char_answer)) {
+                    used.add(char_answer);
+                    char[] new_result_char = checkout(hiddenWordArray, hiddenWordChar.clone(), char_answer);
+                    if (new_result_char == null) {
+                        attemps--;
+                        System.out.println("That letter doesn't appear in the word");
+                    } else if (Arrays.equals(new_result_char, hiddenWordArray)) {
+                        System.out.println("You win!!!");
+                        break;
+                    } else if (!(boolean) Arrays.equals(new_result_char, hiddenWordChar))
+                        hiddenWordChar = new_result_char.clone();
+                } else if (used.contains(char_answer))
+                    System.out.println("You`ve already guessed this letter");
+            }else
+                System.out.println("Please enter a lowercase English letter");
             }
-            else if (Arrays.equals(hiddenWordChar_N, hiddenWordArray)) {
-                System.out.println("You win!");
-                break;
-            } else if (Arrays.equals(hiddenWordChar, hiddenWordChar_N)) {
-                System.out.println("No improvements");
-                attemps--;
-            } else if (!(boolean) Arrays.equals(hiddenWordChar_N, hiddenWordChar)) {
-                hiddenWordChar = hiddenWordChar_N.clone();
-            }
-        }
         if (attemps <= 0)
             System.out.println("You lost!");
     }
